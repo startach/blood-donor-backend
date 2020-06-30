cfeddbaf922bababb4abe4c5af431b92e015744b
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-30 14:49:05 +0300

    merge with master

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,16 +3,18 @@ const router = express.Router();
 const locations = require('./locations');
 
 router.get('/', (req, res) => {
-    res.render("home", {
-      title: 'blood donation organaization',
-      username: "cordinator",
-    })
+  res.render("home", {
+    title: 'blood donation organaization',
+    username: "cordinator",
   })
+})
 
 router.get('/goals', (req, res) => {
   res.render('goals', {
-    goal: '10,000',
-    current:'2033',
+    data: {
+      goal: '10,000',
+      current: '2033',
+    }
   })
 })
 
@@ -20,7 +22,7 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
- router.get('/locations',locations.getAllLocations ); 
+router.get('/locations', locations.getAllLocations);
 
 
 module.exports = router;
