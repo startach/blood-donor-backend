9e4ff1cd97235fd3e5c1abef8d27df0667738b83
moris <moris.rafol@gmail.com>
2020-06-30 16:50:16 +0300

    feat: move routes to router.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors')
-const controllers = require('./controllers')
 const cookieParser = require('cookie-parser')
 const helmet = require('helmet')
 const exphbs = require("express-handlebars");
@@ -10,15 +9,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(controllers)
 app.use(helmet())
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+
+
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
-
-
 app.use(router)
 
 app.engine(
--- src/controllers/router.js
+++ src/controllers/router.js
@@ -1,12 +1,33 @@
+const locations = require("./locations");
 
 const router = require("express").Router()
 
 
-
-router.get("/home", (req,res)=>{
-
-    res.render("login");
+router.get('/', (req, res) => {
+    res.render("home", {
+        title: 'blood donation organaization',
+        username: "cordinator",
+    })
 })
+
+router.get('/goals', (req, res) => {
+    res.render('goals', {
+        goal: '10,000',
+        current:'2033',
+    })
+})
+
+router.get('/login', (req, res) => {
+    res.render('login',{
+        error:"incorrect password",
+        onSubmit:(e)=>{
+            // e.preventDefault()
+            console.log("test")
+        }
+    })
+})
+
+router.get('/locations',locations.getAllLocations );
 
 
 
