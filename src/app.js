8e6c93e38fd25c9eda8e403419635af0b9e03dcd
Marwanrizik91 <marwan.rizik@gmail.com>
2020-06-30 11:56:52 +0300

    added initial mainWrapper -navbar html

--- src/app.js
+++ src/app.js
@@ -15,12 +15,16 @@ app.use(helmet())
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
 
 app.engine(
 	"hbs",
 	exphbs({
 		extname: "hbs",
 		layoutsDir: path.join(__dirname, "views", "layouts"),
+		partialsDir: path.join(__dirname, 'views', 'partials'),
 		defaultLayout: "main"
 	})
 );
