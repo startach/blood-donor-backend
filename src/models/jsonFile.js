05bad4c63d175528a82dfa96d2ff9d2de8c25728
moris <moris.rafol@gmail.com>
2020-08-28 14:15:25 +0300

    feat: jsonFile.js - add delete functions feat: add synchronous variants

--- src/models/jsonFile.js
+++ src/models/jsonFile.js
@@ -17,5 +17,30 @@ function readJsonFile(path) {
         })
     })
 }
+function writeJsonToFileSync(path, data) {
+    fs.writeFileSync(path, JSON.stringify(data))
+}
+function readJsonFileSync(path) {
+    const data = fs.readFileSync(path)
+    return JSON.parse(data);
+}
+function deleteFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.unlink(path, (err) => {
+            if (err) reject(err)
+            else resolve()
+        })
+    })
+}
+function deleteFileSync(path) {
+    fs.unlinkSync(path)
+}
 
-module.exports = {writeJsonToFile,readJsonFile}
+module.exports = {
+    writeJsonToFile,
+    readJsonFile,
+    writeJsonToFileSync,
+    readJsonFileSync,
+    deleteFile,
+    deleteFileSync,
+}
