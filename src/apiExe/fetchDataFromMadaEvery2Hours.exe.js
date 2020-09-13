75c373eec14d66e4d4f055724405588fbb20f37e
Moris R <moris.rafol@gmail.com>
2020-09-13 20:23:32 +0300

    feat: add exe that runs ever 2 and 3 hours

--- src/apiExe/fetchDataFromMadaEvery2Hours.exe.js
+++ src/apiExe/fetchDataFromMadaEvery2Hours.exe.js
@@ -1,13 +1,13 @@
 const schedule = require("node-schedule")
 const fetchAndSendData = require("./fetchAndSendData")
-const successMessage = "next round is in 3 hours. please don't close this window...";
+const successMessage = "next round is in 2 hours. please don't close this window...";
 
 //run the first time
 fetchAndSendData()
     .then(() => console.log(successMessage))
 
 //run every 3 hours
-schedule.scheduleJob('* */3 * * *', function () {
+schedule.scheduleJob('* */2 * * *', function () {
     fetchAndSendData()
         .then(() => console.log(successMessage))
 });
--- src/apiExe/fetchDataFromMadaEveryHour.exe.js
+++ src/apiExe/fetchDataFromMadaEveryHour.exe.js
@@ -1,13 +1,13 @@
 const schedule = require("node-schedule")
 const fetchAndSendData = require("./fetchAndSendData")
-const successMessage = "next round is in 3 hours. please don't close this window...";
+const successMessage = "next round is in an hour. please don't close this window...";
 
 //run the first time
 fetchAndSendData()
     .then(() => console.log(successMessage))
 
 //run every 3 hours
-schedule.scheduleJob('* */3 * * *', function () {
+schedule.scheduleJob('* */1 * * *', function () {
     fetchAndSendData()
         .then(() => console.log(successMessage))
 });
