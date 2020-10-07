e3140f75bcd92c3de55c5a9f7fef1b965e681280
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-07 17:48:45 +0300

    adds get login route

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -7,75 +7,69 @@ const resetPassword = require("./routes/resetPassword");
 const changePassword = require("./routes/changePassword");
 const alerts = require("./routes/alerts");
 const homeMenu = require("./routes/homeMenu");
-const {redirectIfLoggedIn, redirectIfLoggedOut} = require("../middleware/authValidator");
-const router = require("express").Router()
-const apiResponse = require("../models/apiResponse")
+const {
+  redirectIfLoggedIn,
+  redirectIfLoggedOut,
+} = require("../middleware/authValidator");
+const router = require("express").Router();
+const apiResponse = require("../models/apiResponse");
 
-
-router.all(["/",
-    '/changePassword',
+router.all(
+  [
+    "/",
+    "/changePassword",
     /^\/alerts.*/,
     /^\/homeMenu.*/,
     /^\/goals.*/,
     /^\/locations.*/,
-], redirectIfLoggedOut("/login"))
+  ],
+  redirectIfLoggedOut("/login")
+);
 
-router.all([
-    "/login",
-    '/resetPassword',
-], redirectIfLoggedIn("/"))
-
+router.all(["/login", "/resetPassword"], redirectIfLoggedIn("/"));
 
 //routes that require the user to be logged out -----------------------
-router.route('/login')
-    .post(login.post)
+router.route("/login").get(login.get).post(login.post);
 
-router.route('/resetPassword')
-    .post(resetPassword.post)
-
+router.route("/resetPassword").post(resetPassword.post);
 
 //routes that require the user to be logged in ------------
-router.get('/', home.get)
+router.get("/", home.get);
 
-router.route("/changePassword")
-    .get(changePassword.get)
-    .post(changePassword.post)
+router
+  .route("/changePassword")
+  .get(changePassword.get)
+  .post(changePassword.post);
 
-router.route('/goals')
-    .get(goals.get)
-    .post(goals.post);
+router.route("/goals").get(goals.get).post(goals.post);
 
+router.get("/logout", logout.get);
 
-router.get("/logout", logout.get)
+router.get("/alerts", alerts.get);
+router.post("/alerts", alerts.add);
+router.post("/alerts/delete/:id", alerts.delete);
+router.post("/alerts/:id", alerts.post);
 
-router.get('/alerts', alerts.get)
-router.post("/alerts", alerts.add)
-router.post("/alerts/delete/:id", alerts.delete)
-router.post("/alerts/:id", alerts.post)
+router.get("/homeMenu", homeMenu.get);
+router.post("/homeMenu", homeMenu.add);
+router.post("/homeMenu/delete/:id", homeMenu.delete);
+router.post("/homeMenu/:id", homeMenu.post);
 
-router.get('/homeMenu', homeMenu.get)
-router.post("/homeMenu", homeMenu.add)
-router.post("/homeMenu/delete/:id", homeMenu.delete)
-router.post("/homeMenu/:id", homeMenu.post)
-
-router.get("/locations", locations.get)
-
+router.get("/locations", locations.get);
 
 //routes that work all the time ------------------------------------
-router.get('/api/locations', locations.getLocationsApi);
-router.post('/api/locations', locations.setLocationsApi);
-router.get('/iframe/locations', locations.getLocationsIframe);
-router.get('/api/alerts', alerts.getAlertsApi)
-router.get('/api/goals', goals.apiGet)
-router.get('/api/alerts', alerts.getAlertsApi)
-router.get('/api/homeMenu', homeMenu.getApi)
+router.get("/api/locations", locations.getLocationsApi);
+router.post("/api/locations", locations.setLocationsApi);
+router.get("/iframe/locations", locations.getLocationsIframe);
+router.get("/api/alerts", alerts.getAlertsApi);
+router.get("/api/goals", goals.apiGet);
+router.get("/api/alerts", alerts.getAlertsApi);
+router.get("/api/homeMenu", homeMenu.getApi);
 
-
-router.use(( req, res, next) => {
-    apiResponse(res,{code:404,message:"not found"})
-})
+router.use((req, res, next) => {
+  apiResponse(res, { code: 404, message: "not found" });
+});
 router.use((err, req, res, next) => {
-    apiResponse(res,{code:500,message:"server error"})
-})
+  apiResponse(res, { code: 500, message: "server error" });
+});
 module.exports = router;
-
--- src/controllers/routes/login.js
+++ src/controllers/routes/login.js
@@ -1,18 +1,14 @@
-const {firebase} = require("../../database")
-const apiResponse = require("../../models/apiResponse")
+const { firebase } = require("../../database");
 
+exports.get = (req, res) => {
+  if (firebase.auth().currentUser) return res.redirect("/");
+  res.render("login");
+};
 
 exports.post = (req, res) => {
-    // return (res.send("test"))
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
-    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
-    .then(user =>{console.log(user)
-    return     user.getIdToken()})
-     
-    // .then(idToken => {firebase.auth().createSession
-
-    // })
-    
-        .then(() => apiResponse(res,{message:"successfuly"}))
-        .catch(({message}) => apiResponse(res,{message,code:500}))
-} 
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(req.body.email, req.body.password)
+    .then(() => res.redirect("/"))
+    .catch((e) => res.render("login", { error: e.message }));
+};
