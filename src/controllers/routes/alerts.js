638b493b20557a3b1c991bdc4509746c56824dd0
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-08 18:20:15 +0300

    modify delete,edit alerts route

--- src/controllers/routes/alerts.js
+++ src/controllers/routes/alerts.js
@@ -8,7 +8,6 @@ exports.get = async (req, res, next) => {
     data = data.map((x) => ({
       ...x,
       addedDate: x.addedDate.toISOString(),
-      expDate: moment(x.expDate).format("YYYY-MM-DD"),
     }));
     res.render("alerts", { data, selectedNavbarItem: "alerts" });
   } catch (e) {
@@ -17,30 +16,25 @@ exports.get = async (req, res, next) => {
 };
 
 exports.post = async ({ params: { id }, body }, res) => {
-  console.log(body);
-  await ModelsAlerts.edit(id, {
-    title: {
-      he: body.title_he,
-      en: body.title_en,
-      ar: body.title_ar,
-    },
-    context: {
-      he: body.context_he,
-      en: body.context_en,
-      ar: body.context_ar,
-    },
-    bloodType: Object.keys(body)
-      .filter((x) => x.includes("bloodType"))
-      .map((x) => body[x]),
-    expDate: moment(body.expDate, "YYYY-MM-DD").toDate(),
-    addedDate: moment(body.addedDate).toDate(),
-  });
-  res.redirect("/alerts");
+  try {
+    await ModelsAlerts.edit(id, {
+      ...body,
+      addedDate: moment(body.addedDate).toDate(),
+    });
+    apiResponse(res, { message: " edit successfully" });
+  } catch ({ message }) {
+    apiResponse(res, { message, code: 500 });
+  }
 };
 
 exports.delete = async ({ params: { id } }, res) => {
-  await ModelsAlerts.del(id);
-  res.end();
+  try {
+    console.log(id);
+    await ModelsAlerts.del(id);
+    apiResponse(res, { message: " deleted successfully" });
+  } catch ({ message }) {
+    apiResponse(res, { message, code: 500 });
+  }
 };
 
 exports.add = async (req, res) => {
@@ -58,6 +52,7 @@ exports.getAlertsApi = async (req, res) => {
     const data = (await ModelsAlerts.get()) || [];
     apiResponse(res, { data });
   } catch (e) {
+    console.log(e.message);
     apiResponse(res, { message: "server error", code: 500 });
   }
 };
--- src/models/alerts.js
+++ src/models/alerts.js
@@ -1,56 +1,51 @@
-const QueriesAlerts = require('../database/alerts');
-const Joi = require('@hapi/joi');
+const QueriesAlerts = require("../database/alerts");
+const Joi = require("@hapi/joi");
 
 const schema = Joi.object({
-    title: Joi.object({
-        en: Joi.string(),
-        he: Joi.string(),
-        ar: Joi.string()
-    }),
+  title: Joi.object({
+    en: Joi.string(),
+    he: Joi.string(),
+    ar: Joi.string(),
+  }),
 
-    context: Joi.object({
-        en: Joi.string(),
-        he: Joi.string(),
-        ar: Joi.string()
-    }),
-    bloodType: Joi.array().items(Joi.string()),
-    addedDate: Joi.date(),
-    expDate: Joi.date(),
-})
+  context: Joi.object({
+    en: Joi.string(),
+    he: Joi.string(),
+    ar: Joi.string(),
+  }),
+  bloodType: Joi.array().items(Joi.string()),
+  addedDate: Joi.date(),
+});
 
 const edit = async (id, alert) => {
-    if (!id) {
-        throw new Error("id should be defined");
-    }
-    if (schema.validate(alert).error)
-        throw new Error (schema.validate(alert).error.message);
+  console.log(id, alert);
+  if (!id) {
+    throw new Error("id should be defined");
+  }
+  if (schema.validate(alert).error)
+    throw new Error(schema.validate(alert).error.message);
+  return await QueriesAlerts.edit(id, alert);
+};
 
-    return await QueriesAlerts.edit(id, alert);
-}
-
-
-const get = QueriesAlerts.get
+const get = QueriesAlerts.get;
 
 const del = async (id) => {
-    if (!id) {
-        throw new Error("id should be defined");
-    }
+  if (!id) {
+    throw new Error("id should be defined");
+  }
 
-    return await QueriesAlerts.del(id)
-
-}
+  return await QueriesAlerts.del(id);
+};
 
 const add = async (alert) => {
-    if (schema.validate(alert).error)
-        throw new Error(schema.validate(alert).error);
-    return await QueriesAlerts.add(alert);
-}
-
-
+  if (schema.validate(alert).error)
+    throw new Error(schema.validate(alert).error);
+  return await QueriesAlerts.add(alert);
+};
 
 module.exports = {
-    edit,
-    get,
-    del,
-    add,
-}
+  edit,
+  get,
+  del,
+  add,
+};
