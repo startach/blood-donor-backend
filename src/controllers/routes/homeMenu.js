3aaaf9e4fb676d696903f3e704fc68b9c493e20d
hadi kalil <hadikalilweb@gmail.com>
2020-10-13 18:02:44 +0300

    edit delet

--- src/controllers/routes/homeMenu.js
+++ src/controllers/routes/homeMenu.js
@@ -1,68 +1,51 @@
-const ModelsHomeMenuItems = require('../../models/home_menu_items');
-const apiResponse = require("../../models/apiResponse")
-
+const ModelsHomeMenuItems = require("../../models/home_menu_items");
+const apiResponse = require("../../models/apiResponse");
 
 exports.get = async (req, res, next) => {
-
-    try {
-        const data = await ModelsHomeMenuItems.get()
-        res.render("homeMenu", { data, selectedNavbarItem: 'homeMenu' })
-    } catch {
-        next()
-    }
-}
+  try {
+    const data = await ModelsHomeMenuItems.get();
+    res.render("homeMenu", { data, selectedNavbarItem: "homeMenu" });
+  } catch {
+    next();
+  }
+};
 
 exports.post = async ({ params: { id }, body }, res) => {
+  try {
+    await ModelsHomeMenuItems.edit(id, {
+        ...body,
 
-    try {
-        await ModelsHomeMenuItems.edit(id, {
-            title: {
-                he: body.title_he,
-                en: body.title_en,
-                ar: body.title_ar,
-
-            },
-            src: body.src,
-            redirectionLink: body.redirectionLink
-
-        })
-        res.redirect("/homeMenu")
-    } catch ({ message }) {
-        console.error(message)
-    }
-
-}
+     
+    });
+    apiResponse(res, { message: " edit successfully" });
+  } catch ({ message }) {
+    apiResponse(res, { message, code: 500 });
+  }
+};
 
 exports.delete = async ({ params: { id } }, res) => {
-    try {
-        await ModelsHomeMenuItems.del(id)
-        res.end()
-    } catch ({ message }) {
-        console.error(message)
-    }
-}
+  try {
+    await ModelsHomeMenuItems.del(id);
+    apiResponse(res, { message: " deleted successfully" });
+  } catch ({ message }) {
+    apiResponse(res, { message, code: 500 });
+  }
+};
 
 exports.add = async ({ body }, res) => {
-
-    try {
-        await ModelsHomeMenuItems.add(
-          body
-        )
-        apiResponse(res, { message: " edit successfully" });
-    } catch ({ message }) {
-      apiResponse(res, { message, code: 500 });
-    }
-}
-
-
+  try {
+    await ModelsHomeMenuItems.add(body);
+    apiResponse(res, { message: " edit successfully" });
+  } catch ({ message }) {
+    apiResponse(res, { message, code: 500 });
+  }
+};
 
 exports.getApi = async (req, res) => {
-
-    try {
-        const data = await ModelsHomeMenuItems.get()
-        apiResponse(res,{data})
-    } catch (e) {
-        apiResponse(res,{message:"server error",code:500})
-    }
-
-}
+  try {
+    const data = await ModelsHomeMenuItems.get();
+    apiResponse(res, { data });
+  } catch (e) {
+    apiResponse(res, { message: "server error", code: 500 });
+  }
+};
